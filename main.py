933fcc5d411d62c5279606416c645cf39b64eb39
mimmol99 <92627826+mimmol99@users.noreply.github.com>
2024-07-17 12:39:24 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from langchain_core.runnables.graph_png import PngDrawer
 import subprocess
 import sys
 import inspect
+import ast
 
 
 def install(package):
@@ -42,7 +43,7 @@ You need to review the given code following PEP8 guidelines and potential bugs \
 and point out issues as bullet list. \
 Code:\n{}"
 
-coder_start = "You are a Coder specialized in {}. \
+coder_start = "You are a Coder specialized in {} and you have to solve this problem:{}. \
 Improve the given code given the following guidelines. Guideline:\n{} \n \
 Code:\n{} \n \
 Output just the improved code and nothing else (don't output '''python or similar)."
@@ -59,11 +60,16 @@ classify_errors = "You are a errors classifier. If the error passed is about \
 a module or package to be installed, return the list of packages that must be installed \
 using pip(e.g. '[torch, transformers]'), else return an empty list (e.g. '[]'). Error: {}"
 
-error_fix_prompt = "You are a skilled programmer. Fix the following errors in the code:\nError:\n{} \nCode:\n{} \nOutput just the fixed code and nothing else (don't output '''python or similar)."
+error_fix_prompt = "You are a skilled programmer. Fix the following errors \
+ in the code:\nError:\n{} \nCode:\n{} \nOutput just the fixed code \
+ and nothing else (don't output '''python or similar)."
 
-error_fix_package = "I just tried to install {} using subprocess.check_call([sys.executable, '-m', 'pip', 'install', {}]),but it returns this error: {} return only the correct package name "
+error_fix_package = "I just tried to install {} using \
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', {}]),\
+    but it returns this error: {} return only the correct package name "
 
 class GraphState(TypedDict):
+    problem: Optional[str]
     actual_code: Optional[str]
     code: Optional[str]
     code_compare: Optional[str]
@@ -99,11 +105,12 @@ def handle_reviewer(state: Dict) -> Dict:
 
 def handle_coder(state: Dict) -> Dict:
     print_actual_func()
+    problem = state.get('problem').strip()
     history = state.get('history', '').strip()
     feedback = state.get('feedback', '').strip()
     code_input = state.get('code', '').strip()
     specialization = state.get('specialization', '').strip()
-    code = generate_conversation(coder_start.format(specialization, feedback, code_input))
+    code = generate_conversation(coder_start.format(specialization,problem,feedback, code_input))
     state.update({'history': history + '\n CODER:\n' + code, 'code': code})
     return state
 
@@ -112,7 +119,7 @@ def handle_executor(state: Dict) -> Dict:
     code_input = state.get('code', '').strip()
     error_output = None
     try:
-        output = exec(code)
+        output = exec(code_input)
     except Exception as e:
         print(f"Found error:",e)
         error_output = str(e)
@@ -132,10 +139,9 @@ def handle_installing_package(state: Dict) -> Dict:
     print_actual_func()
     error = state.get('error', None)
     if error is None: return state 
-    packages_to_install = generate_conversation(classify_errors.format(error))
-    packages_to_install = eval(packages_to_install.replace("'", " ").replace("`"," "))
-    #packages_to_install = packages_to_install.replace("'", " ").replace("`"," ").split(",")
-    #print(packages_to_install)
+    packages_to_install_str= generate_conversation(classify_errors.format(error))
+    packages_to_install = ast.literal_eval(packages_to_install_str)
+
     for package in packages_to_install:
         error = install(package)
         print(f"installed {package}")
@@ -211,10 +217,7 @@ workflow.add_conditional_edges(
 
 
 workflow.set_entry_point('handle_reviewer')
-#workflow.add_edge('handle_coder', 'handle_reviewer')
 workflow.add_edge('handle_coder', 'handle_executor')
-#workflow.add_edge('handle_executor', 'handle_error')
-#workflow.add_edge('handle_executor', 'handle_installing_package')
 workflow.add_edge('handle_error','handle_reviewer')
 workflow.add_edge('handle_installing_package', 'handle_reviewer')
 workflow.add_edge('handle_result', langgraph.graph.END)
@@ -226,12 +229,11 @@ file_name = generate_conversation(f"Output just a 'file_name.py' that well repre
 code = generate_conversation(problem)
 
 app = workflow.compile()
-conversation = app.invoke({"history": code, "code": code, 'actual_code': code, "specialization": specialization, 'iterations': 0,'error':None})
+conversation = app.invoke({"problem":problem,"history": code, "code": code, 'actual_code': code, "specialization": specialization, 'iterations': 0,'error':None})
 
-#print(conversation['code_compare'])
+print(conversation['code_compare'])
 
 new_code = conversation["code"]
-#print("Actual code", code, "\n\n\n New code:", new_code)
 
 with open(file_name, 'w') as f:
     f.write(new_code)
